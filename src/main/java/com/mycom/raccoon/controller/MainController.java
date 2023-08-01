130621c7df13df11e5b0a73b95dfbca0c36cd071
zzzz1031 <zzzz1031@naver.com>
2023-08-01 16:05:38 +0900

    change project name, update git info.

---
+++ src/main/java/com/mycom/raccoon/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.mycom.raccoon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/")
+  public String index(HttpServletRequest request, HttpServletResponse response, Model model) {
+    model.addAttribute("id", "user");
+    return "main";
+  }
+}
