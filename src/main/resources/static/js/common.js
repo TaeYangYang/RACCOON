fcec4ed92c635929990dbc0f7977b945e15d160b
TaeYang <zzzz1031@naver.com>
2023-08-22 19:25:52 +0900

    아이디, 패스워드찾기 개발중

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,28 @@
+/* 공통 사용되는 JS */
+
+/**
+ * 인증번호 전송
+ * @param celno
+ * @param callback
+ */
+function sendAuthCodeSMS(celno, callback){
+  if(celno === null || celno === '' || celno === undefined){
+    alert('연락처를 입력해주세요.');
+  } else[
+    // 서버에 axios post요청으로 인증번호 발송
+    axios.post(`/user/sendAuthSMS`, {celno: celno}, {withCredentials: false}) // url, parameter, withCredentails(default : false / CORS 또는 쿠키 정책관련)
+        .then(res => { // 응답 받는경우
+          if(res.data.length === 6){
+            alert('인증번호가 발송되었습니다.');
+            callback(res.data);
+          } else{
+            alert('[SMS 발송에러]');
+          }
+        })
+        .catch(err => { // 에러처리
+          console.log(err);
+          alert('ERROR');
+        })
+  ]
+
+}
