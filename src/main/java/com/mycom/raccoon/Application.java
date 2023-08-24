822292ed4d55758f2f90702eb18186a8d5d49046
TaeYang <zzzz1031@naver.com>
2023-08-24 23:52:18 +0900

    Application.java 수정

--- src/main/java/com/mycom/raccoon/Application.java
+++ src/main/java/com/mycom/raccoon/Application.java
@@ -2,10 +2,11 @@ package com.mycom.raccoon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
